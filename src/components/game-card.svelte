5f0481b44e26f59b6b7fdc46606e6a0d658b78a2
Itachi-MacOS <warrior.006@inbox.ru>
2024-08-02 03:48:09 +0500

    game cards

---
+++ src/components/game-card.svelte
@@ -0,0 +1,28 @@
+<script>
+
+</script>
+<div class="px-2 pt-2 pb-3 rounded-[14px] max-w-[290px] group hover:bg-[#464648] cursor-pointer transition-all">
+  <div class="w-[257px] mx-auto h-[257px] bg-slate-500 rounded-[10px]">
+
+  </div>
+  <div class="mt-2 group-hover:hidden transition-all">
+    <div class="leading-[120%]">
+      MOBOX
+    </div>
+    <div class="flex items-center justify-between">
+      <div class="text-[14px] leading-[120%] text-gray-400">Casual, Metaverse, Adventure</div>
+      <div class="flex -space-x-2">
+        <img width="20" height="20" src="/solana-icon.svg" alt="">
+        <img width="20" height="20" src="/ton-icon.svg" alt="">
+      </div>
+    </div>
+  </div>
+  <button class="hidden hover:bg-[#1CC141] w-full group/btn mt-2 font-medium bg-[#fff] group-hover:flex items-center justify-center space-x-2 text-black py-[10px] active:scale-95 active:bg-[#17B23A] transition-all px-[36px] rounded-[12px]">
+    <svg class="group-hover/btn:text-white" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+      <path d="M7 4V20L20 12L7 4Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+    <span class="group-hover/btn:text-white">
+      Play
+    </span>
+  </button>
+</div>
