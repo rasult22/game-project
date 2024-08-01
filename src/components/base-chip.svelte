d8c88b630a70c9d44a0edaf608f8d190df0bfb53
Itachi-MacOS <warrior.006@inbox.ru>
2024-08-02 01:04:32 +0500

    chips and filters

---
+++ src/components/base-chip.svelte
@@ -0,0 +1,19 @@
+<script>
+  export let active = false
+</script>
+<button class="chip space-x-[6px] text-[14px]" class:chip-active={active}>
+  <span>
+    <slot name="text">
+      TON
+    </slot>
+  </span>
+</button>
+
+<style>
+  .chip {
+    @apply border py-2 px-[10px] rounded-[12px] flex active:bg-white active:text-black active:scale-95 transition-all;
+  }
+  .chip-active {
+    @apply border py-2 px-[10px] rounded-[12px] flex bg-white text-black active:scale-95 transition-all;
+  }
+</style>
---
+++ src/components/network-chip.svelte
@@ -0,0 +1,22 @@
+<script>
+  export let active = false
+</script>
+<button class="chip space-x-[6px] text-[14px]" class:chip-active={active}>
+  <slot name="img">
+    <img src="/ton-icon.svg" alt="">
+  </slot>
+  <span>
+    <slot name="text">
+      TON
+    </slot>
+  </span>
+</button>
+
+<style>
+  .chip {
+    @apply border p-2 rounded-[12px] flex items-center active:bg-white active:text-black active:scale-95 transition-all;
+  }
+  .chip-active {
+    @apply border p-2 rounded-[12px] flex bg-white text-black active:scale-95 transition-all;
+  }
+</style>
