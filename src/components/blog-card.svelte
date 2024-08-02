e09a2a51a8a3aa58554cd0b442a83167395112f9
Itachi-MacOS <warrior.006@inbox.ru>
2024-08-02 07:14:41 +0500

    blog card

---
+++ src/components/blog-card.svelte
@@ -0,0 +1,10 @@
+<div class="bg-[#272727] py-2 pl-2 pr-4 flex space-x-[36px] max-w-[912px] rounded-[12px]">
+  <img class="max-w-[410px]" src="/blog-cover.png" alt="">
+  <div class="w-full justify-center flex flex-col text-white">
+    <div class="text-[24px] leading-[120%] font-semibold">The U.S. proposed to study the impact of bitcoin mining on the environment</div>
+    <div class="text-[#B0B0B2] leading-[120%] mt-4">
+      U.S. lawmakers have introduced a bill that would require the Environmental Protection Agency to conduct a comprehensive study of the environmental impact of the mining industry.
+    </div>
+    <button class="flex text-[20px] mt-4">Read more</button>
+  </div>
+</div>
