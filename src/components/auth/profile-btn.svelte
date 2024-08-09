07643598d7c10c26f77954eba863bf01b0f28965
Itachi-MacOS <warrior.006@inbox.ru>
2024-08-09 06:59:40 +0500

    add

--- src/components/auth/profile-btn.svelte
+++ src/components/auth/profile-btn.svelte
@@ -4,7 +4,7 @@
   export let profile_pic
 </script>
 <a href="/game-project/profile" class="flex group items-center -space-x-2 cursor-pointer">
-  <div class="bg-[#2A2A2A] group-hover:bg-[#444] cursor-pointer space-x-2 flex items-center px-3 py-1 rounded-[12px]">
+  <div class="bg-[#2A2A2A] group-hover:bg-[#444] border border-[#F78E7B] cursor-pointer space-x-2 flex items-center px-3 py-1 rounded-[12px]">
     <div>
       <div class="text-[14px]">{name}</div>
       <div class="text-[12px] font-medium">{address}</div>
