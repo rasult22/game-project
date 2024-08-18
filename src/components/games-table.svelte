46f2f1a53d133e1258c423fd8a4d0906c5f65fc6
Itachi-MacOS <warrior.006@inbox.ru>
2024-08-19 01:38:18 +0500

    adaptive gamehub

--- src/components/games-table.svelte
+++ src/components/games-table.svelte
@@ -6,7 +6,7 @@
 </script>
 
 <div class="bg-transparent space-y-3 w-full text-white p-4 rounded-lg">
-  <div class="grid border-b border-[#787575] mb-4 text-center grid-cols-8 items-center py-2 text-sm group d">
+  <div class="grid sm:hidden border-b border-[#787575] mb-4 text-center grid-cols-8 md:grid-cols-6 items-center py-2 text-sm group d">
     <div class="col-span-2 flex space-x-[24px]">
       <div class="text-left text-[#787575]">Rank</div>
       <div class="text-left">Name</div>
@@ -15,11 +15,11 @@
     <div>Session Lenght</div>
     <div>ARPDAU</div>
     <div>Churn Rate</div>
-    <div class="text-center">Token Volume</div>
-    <div class="text-center">Market cap</div>
+    <div class="text-center md:hidden">Token Volume</div>
+    <div class="text-center md:hidden">Market cap</div>
   </div>  
   {#each data as item, index}
-    <div class="grid grid-cols-8 items-center py-2 text-sm group d">
+    <div class="grid grid-cols-8 md:grid-cols-6 sm:grid-cols-2 items-center py-2 text-sm group">
       <div class="col-span-2 flex items-center gap-3">
         <span class="w-14 text-left">
           {#if index === 0}
@@ -58,36 +58,36 @@
           </div>
         </div>
       </div>
-      <div class="text-center">
+      <div class="text-center sm:hidden">
         <span>{item.retention}</span>
         <div class="flex items-center justify-center text-red-500">
           <ChevronDown class="w-4 h-4" />
           <span>{item.retentionChange}</span>
         </div>
       </div>
-      <div class="text-center">
+      <div class="text-center sm:hidden">
         <span>{item.session}</span>
         <div class="flex items-center justify-center text-green-500">
           <ChevronUp class="w-4 h-4" />
           <span>{item.sessionChange}</span>
         </div>
       </div>
-      <div class="text-center">
+      <div class="text-center sm:hidden">
         <span>{item.arp_dau}</span>
         <div class="flex items-center justify-center text-green-500">
           <ChevronUp class="w-4 h-4" />
           <span>{item.arp_dau_change}</span>
         </div>
       </div>
-      <div class="text-center">
+      <div class="text-center sm:hidden">
         <span>{item.churn}</span>
         <div class="flex items-center justify-center text-green-500">
           <ChevronUp class="w-4 h-4" />
           <span>{item.churnChange}</span>
         </div>
       </div>
-      <div class="text-center">{item.token_volume}</div>
-      <div class="text-center">{item.market_cap}</div>
+      <div class="text-center md:hidden sm:hidden">{item.token_volume}</div>
+      <div class="text-center md:hidden sm:hidden">{item.market_cap}</div>
     </div>
   {/each}
 </div>
