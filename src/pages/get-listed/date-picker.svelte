880aa42b0b355b3e6cdecb95e3d390ada3add56d
rasult22 <warrior.006@inbox.ru>
2024-09-25 21:46:50 +0500

    get listed pt.1

---
+++ src/pages/get-listed/date-picker.svelte
@@ -0,0 +1,31 @@
+<script>
+import flatpickr from "flatpickr";
+import { createEventDispatcher, onMount } from "svelte";
+import 'flatpickr/dist/flatpickr.min.css';
+const dispatch = createEventDispatcher()
+let fp;
+onMount(() => {
+  fp = flatpickr('#date-picker', {
+    onChange: (dates, currentDateString, self, data) => {
+      dispatch('date-select', currentDateString)
+      console.log(dates, currentDateString, self, data)
+    }
+  })
+})
+</script>
+<div class="relative">
+  <svg class="absolute cursor-pointer left-3 top-[50%] translate-y-[-50%]" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M6.66667 1.66699C7.1269 1.66699 7.5 2.04009 7.5 2.50033V3.33366H12.5V2.50033C12.5 2.04009 12.8731 1.66699 13.3333 1.66699C13.7936 1.66699 14.1667 2.04009 14.1667 2.50033V3.33366H15C16.3807 3.33366 17.5 4.45295 17.5 5.83366V15.8337C17.5 17.2144 16.3807 18.3337 15 18.3337H5C3.61929 18.3337 2.5 17.2144 2.5 15.8337V5.83366C2.5 4.45295 3.61929 3.33366 5 3.33366H5.83333V2.50033C5.83333 2.04009 6.20643 1.66699 6.66667 1.66699ZM5.83333 5.00033H5C4.53976 5.00033 4.16667 5.37342 4.16667 5.83366V8.33366H15.8333V5.83366C15.8333 5.37342 15.4602 5.00033 15 5.00033H14.1667V5.83366C14.1667 6.2939 13.7936 6.66699 13.3333 6.66699C12.8731 6.66699 12.5 6.2939 12.5 5.83366V5.00033H7.5V5.83366C7.5 6.2939 7.1269 6.66699 6.66667 6.66699C6.20643 6.66699 5.83333 6.2939 5.83333 5.83366V5.00033ZM15.8333 10.0003H4.16667V15.8337C4.16667 16.2939 4.53976 16.667 5 16.667H15C15.4602 16.667 15.8333 16.2939 15.8333 15.8337V10.0003ZM8.33333 12.5003C8.33333 12.0401 8.70643 11.667 9.16667 11.667H10C10.4602 11.667 10.8333 12.0401 10.8333 12.5003V15.0003C10.8333 15.4606 10.4602 15.8337 10 15.8337C9.53976 15.8337 9.16667 15.4606 9.16667 15.0003V13.3337C8.70643 13.3337 8.33333 12.9606 8.33333 12.5003Z" fill="#464648"/>
+  </svg>
+  <input id="date-picker" type="text" required class="my-input" placeholder="--/--/----">
+  <svg class="absolute cursor-pointer right-3 top-[50%] translate-y-[-50%]" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M3.5312 12.4717C3.79155 12.7321 4.21366 12.7321 4.47401 12.4717L8.0026 8.94313L11.5312 12.4717C11.7915 12.7321 12.2137 12.7321 12.474 12.4717C12.7344 12.2114 12.7344 11.7893 12.474 11.5289L8.94541 8.00033L12.474 4.47173C12.7344 4.21138 12.7344 3.78927 12.474 3.52892C12.2137 3.26857 11.7915 3.26857 11.5312 3.52892L8.0026 7.05752L4.47401 3.52892C4.21366 3.26857 3.79155 3.26857 3.5312 3.52892C3.27085 3.78927 3.27085 4.21138 3.5312 4.47173L7.0598 8.00033L3.5312 11.5289C3.27085 11.7893 3.27085 12.2114 3.5312 12.4717Z" fill="#868E96"/>
+  </svg>    
+</div>
+
+
+<style>
+  .my-input {
+    @apply px-10 py-[14px] rounded-[12px] block w-full placeholder:text-[#888888] bg-[#141414] border border-[#323234] hover:outline-none
+  }
+</style>
---
+++ src/pages/get-listed/get-listed-form.svelte
@@ -0,0 +1,247 @@
+<script>
+  import DatePicker from "./date-picker.svelte";
+</script>
+
+<div class="text-[36px] font-Oxanium font-medium uppercase">
+  Send request
+</div>
+<div class="text-[#888888]">Fill out the form below and our manager will contact you as soon as possible</div>
+<form class="mt-6">
+  <!-- project -->
+  <div class="flex flex-col w-[50%] md:w-full sm:w-full mt-[32px]">
+    <label for="project-name">
+      Project Name
+      <span class="text-[#FA5252]">*</span>
+    </label>
+    <input type="text" id="project-name" required class="my-input mt-[6px]" placeholder="Enter the project name">
+  </div>
+  <!-- studio -->
+  <div class="flex flex-col w-[50%] md:w-full sm:w-full mt-[32px]">
+    <label for="studio-name">
+      Studio Name
+    </label>
+    <input type="text" id="studio-name" class="my-input mt-[6px]" placeholder="Enter the studio name">
+  </div>
+
+
+  <!-- Application type -->
+  <div class="flex flex-col w-[50%] md:w-full sm:w-full mt-[32px]">
+    <label>
+      Application Type
+      <span class="text-[#FA5252]">*</span>
+    </label>
+    <div class="flex gap-4 mt-3 select-none">
+      <label class="flex items-center space-x-2 cursor-pointer">
+        <!-- The wrapper span will contain the custom checkbox -->
+        <span class="relative w-[20px] h-[20px] ">
+          <input type="checkbox" class="opacity-0 absolute w-full h-full peer">
+          <svg xmlns="http://www.w3.org/2000/svg" class="w-[16px] h-[16px] hidden absolute left-[50%] translate-x-[-50%] z-[2] top-[50%] translate-y-[-50%] peer-checked:block" width="16" height="16" viewBox="0 0 16 16" fill="none">
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M13.8021 4.19526C14.0625 4.45561 14.0625 4.87772 13.8021 5.13807L7.13547 11.8047C6.87512 12.0651 6.45301 12.0651 6.19266 11.8047L2.85932 8.4714C2.59898 8.21106 2.59898 7.78894 2.85932 7.5286C3.11967 7.26825 3.54178 7.26825 3.80213 7.5286L6.66406 10.3905L12.8593 4.19526C13.1197 3.93491 13.5418 3.93491 13.8021 4.19526Z" fill="white"/>
+          </svg>
+          <div class="w-[20px] h-[20px] peer-checked:bg-[#F97C0E] border-2 border-[#DEE2E6] peer-checked:border-transparent rounded-[4px] absolute top-0 left-0 z-[1]">
+          </div>
+        </span>
+        <span>Game</span>
+      </label>
+      <label class="flex items-center space-x-2 cursor-pointer">
+        <!-- The wrapper span will contain the custom checkbox -->
+        <span class="relative w-[20px] h-[20px] ">
+          <input type="checkbox" class="opacity-0 absolute w-full h-full peer">
+          <svg xmlns="http://www.w3.org/2000/svg" class="w-[16px] h-[16px] hidden absolute left-[50%] translate-x-[-50%] z-[2] top-[50%] translate-y-[-50%] peer-checked:block" width="16" height="16" viewBox="0 0 16 16" fill="none">
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M13.8021 4.19526C14.0625 4.45561 14.0625 4.87772 13.8021 5.13807L7.13547 11.8047C6.87512 12.0651 6.45301 12.0651 6.19266 11.8047L2.85932 8.4714C2.59898 8.21106 2.59898 7.78894 2.85932 7.5286C3.11967 7.26825 3.54178 7.26825 3.80213 7.5286L6.66406 10.3905L12.8593 4.19526C13.1197 3.93491 13.5418 3.93491 13.8021 4.19526Z" fill="white"/>
+          </svg>
+          <div class="w-[20px] h-[20px] peer-checked:bg-[#F97C0E] border-2 border-[#DEE2E6] peer-checked:border-transparent rounded-[4px] absolute top-0 left-0 z-[1]">
+          </div>
+        </span>
+        <span>Application</span>
+      </label>
+      <label class="flex items-center space-x-2 cursor-pointer">
+        <!-- The wrapper span will contain the custom checkbox -->
+        <span class="relative w-[20px] h-[20px] ">
+          <input type="checkbox" class="opacity-0 absolute w-full h-full peer">
+          <svg xmlns="http://www.w3.org/2000/svg" class="w-[16px] h-[16px] hidden absolute left-[50%] translate-x-[-50%] z-[2] top-[50%] translate-y-[-50%] peer-checked:block" width="16" height="16" viewBox="0 0 16 16" fill="none">
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M13.8021 4.19526C14.0625 4.45561 14.0625 4.87772 13.8021 5.13807L7.13547 11.8047C6.87512 12.0651 6.45301 12.0651 6.19266 11.8047L2.85932 8.4714C2.59898 8.21106 2.59898 7.78894 2.85932 7.5286C3.11967 7.26825 3.54178 7.26825 3.80213 7.5286L6.66406 10.3905L12.8593 4.19526C13.1197 3.93491 13.5418 3.93491 13.8021 4.19526Z" fill="white"/>
+          </svg>
+          <div class="w-[20px] h-[20px] peer-checked:bg-[#F97C0E] border-2 border-[#DEE2E6] peer-checked:border-transparent rounded-[4px] absolute top-0 left-0 z-[1]">
+          </div>
+        </span>
+        <span>Metaverse</span>
+      </label>
+    </div>
+  </div>
+
+  <!-- genre -->
+  <div class="flex flex-col w-[50%] md:w-full sm:w-full mt-[32px]">
+    <label for="game-genre">
+      Genre
+      <span class="text-[#FA5252]">*</span>
+    </label>
+    <input type="text" id="game-genre" required class="my-input mt-[6px]" placeholder="Enter game genre ">
+  </div>
+
+   <!-- project description -->
+   <div class="flex flex-col w-[50%] md:w-full sm:w-full mt-[32px]">
+    <label for="project-desc">
+      Project Description
+    </label>
+    <div class="text-[14px] text-[#888888] mt-1">Please provide a detailed description of your project</div>
+    <textarea type="text" id="project-desc" class="my-input mt-3 min-h-[380px]" />
+  </div>
+
+   <!-- project link -->
+   <div class="flex flex-col w-[50%] md:w-full sm:w-full mt-[32px]">
+    <label for="project-link">
+      Link to Project
+      <span class="text-[#FA5252]">*</span>
+    </label>
+    <input type="text" id="project-link" required class="my-input mt-[6px]" placeholder="Website, Telegram app link, etc.">
+  </div>
+
+   <!-- social media -->
+   <div class="flex flex-col w-[50%] md:w-full sm:w-full mt-[32px]">
+    <label>
+      Social Media
+    </label>
+    <div class="grid grid-cols-2 gap-3 mt-3">
+      <div class="flex flex-col w-full">
+        <label class="flex gap-[6px]">
+          <img width="24" src="/media/x.svg" alt="">
+          <span>
+            X (formerly Twitter)
+          </span>
+        </label>
+        <input type="text" required class="my-input mt-[10px]" placeholder="Enter URL">
+      </div>
+      <div class="flex flex-col w-full">
+        <label class="flex gap-[6px]">
+          <img width="24" src="/media/discord.svg" alt="">
+          <span>
+            Discord
+          </span>
+        </label>
+        <input type="text" required class="my-input mt-[10px]" placeholder="Enter URL">
+      </div>
+      <div class="flex flex-col w-full">
+        <label class="flex gap-[6px]">
+          <img width="24" src="/media/facebook.svg" alt="">
+          <span>
+            Facebook
+          </span>
+        </label>
+        <input type="text" required class="my-input mt-[10px]" placeholder="Enter URL">
+      </div>
+      <div class="flex flex-col w-full">
+        <label class="flex gap-[6px]">
+          <img width="24" src="/media/instagram.svg" alt="">
+          <span>
+            Instagram
+          </span>
+        </label>
+        <input type="text" required class="my-input mt-[10px]" placeholder="Enter URL">
+      </div>
+      <div class="flex flex-col w-full">
+        <label class="flex gap-[6px]">
+          <img width="24" src="/media/telegram.svg" alt="">
+          <span>
+            Telegram
+          </span>
+        </label>
+        <input type="text" required class="my-input mt-[10px]" placeholder="Enter URL">
+      </div>
+      <div class="flex flex-col w-full">
+        <label class="flex gap-[6px]">
+          <span>
+            Other
+          </span>
+        </label>
+        <input type="text" required class="my-input mt-[10px]" placeholder="Enter URL">
+      </div>
+    </div>
+  </div>
+
+
+   <!-- project status -->
+   <div class="flex flex-col w-[50%] md:w-full sm:w-full mt-[32px]">
+    <label>
+      Project Status
+      <span class="text-[#FA5252]">*</span>
+    </label>
+    <div class="flex gap-3 mt-3">
+      <div class="inline-flex gap-2 items-center select-none">
+        <label class="relative flex items-center cursor-pointer" for="alpha">
+          <input
+            checked
+            name="color"
+            type="radio"
+            class="peer h-5 w-5 cursor-pointer appearance-none rounded-full border border-slate-300 checked:border-amber-400 transition-all"
+            id="alpha"
+          />
+          <span class="absolute bg-amber-600 w-3 h-3 rounded-full opacity-0 peer-checked:opacity-100 transition-opacity duration-200 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"></span>
+        </label>
+        <label class="cursor-pointer" for="alpha">
+          Alpha/Beta
+        </label>
+      </div>
+      <div class="inline-flex gap-2 items-center select-none">
+        <label class="relative flex items-center cursor-pointer" for="launch">
+          <input
+            name="color"
+            type="radio"
+            class="peer h-5 w-5 cursor-pointer appearance-none rounded-full border border-slate-300 checked:border-amber-400 transition-all"
+            id="launch"
+          />
+          <span class="absolute bg-amber-600 w-3 h-3 rounded-full opacity-0 peer-checked:opacity-100 transition-opacity duration-200 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"></span>
+        </label>
+        <label class="cursor-pointer" for="launch">
+          Soft Launch
+        </label>
+      </div>
+      <div class="inline-flex gap-2 items-center select-none">
+        <label class="relative flex items-center cursor-pointer" for="live">
+          <input
+            name="color"
+            type="radio"
+            class="peer h-5 w-5 cursor-pointer appearance-none rounded-full border border-slate-300 checked:border-amber-400 transition-all"
+            id="live"
+          />
+          <span class="absolute bg-amber-600 w-3 h-3 rounded-full opacity-0 peer-checked:opacity-100 transition-opacity duration-200 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"></span>
+        </label>
+        <label class="cursor-pointer" for="live">
+          Live
+        </label>
+      </div>
+      <div class="inline-flex gap-2 items-center select-none">
+        <label class="relative flex items-center cursor-pointer" for="maintenance">
+          <input
+            name="color"
+            type="radio"
+            class="peer h-5 w-5 cursor-pointer appearance-none rounded-full border border-slate-300 checked:border-amber-400 transition-all"
+            id="maintenance"
+          />
+          <span class="absolute bg-amber-600 w-3 h-3 rounded-full opacity-0 peer-checked:opacity-100 transition-opacity duration-200 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"></span>
+        </label>
+        <label class="cursor-pointer" for="maintenance">
+          Maintenance
+        </label>
+      </div>
+    </div>
+
+  </div>
+  <!-- project link -->
+ <div class="flex flex-col w-[50%] md:w-full sm:w-full mt-[32px]">
+   <label>
+     Project Launch Date
+     <span class="text-[#FA5252]">*</span>
+   </label>
+   <DatePicker on:date-select={(e) => {
+    console.log('hello', e.detail)
+   }}/>
+ </div>
+
+</form>
+
+<style>
+  .my-input {
+    @apply px-4 py-[14px] rounded-[12px] placeholder:text-[#888888] bg-[#141414] border border-[#323234] hover:outline-none
+  }
+</style>
