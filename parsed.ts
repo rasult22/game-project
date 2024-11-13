54608050ce6b83b54796da0b6251999d5e2e696a
rasult22 <warrior.006@inbox.ru>
2024-11-14 01:52:52 +0500

    123

--- parsed.ts
+++ parsed.ts
@@ -12,7 +12,7 @@ export default [
     "networks": [
       "aptos"
     ],
-    "players_count": 4505,
+    "players_count": '1100',
     "star_rating": {
       "collection_id": "08c1027f-4984-487a-a968-3c710ef2bd00",
       "collection_type": "games",
