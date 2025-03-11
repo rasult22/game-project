23a295be7642ce92976f87d136448b6c1bdbfa1b
rasult22 <warrior.006@inbox.ru>
2025-03-12 00:57:52 +0500

    add success modal for registration form; manage request state during submission

--- src/pages/get-listed-v2/get-listed-form.svelte
+++ src/pages/get-listed-v2/get-listed-form.svelte
@@ -1,14 +1,17 @@
 <script>
+import SucceedModal from './success-modal.svelte';
 let organization_name = '';
 let link_to_project = '';
 let first_name = '';
 let last_name = '';
 let email = '';
+let requestState = 'initial'
 
 const onSubmit = async (e) => {
 
   if (organization_name && email && link_to_project && first_name && last_name) {
     try {
+      requestState = 'loading';
       await fetch('https://api.bizzon.io/api/organization/registration', {
         method: 'POST',
         headers: {
@@ -22,17 +25,28 @@ const onSubmit = async (e) => {
           last_name
         })
       })
+      requestState = 'done';
     } catch (error) {
       console.error(error)
     }
   }
 }
 </script>
+
+{#if requestState === 'done'}
+  <SucceedModal email={email} on:close={() => {
+    requestState = 'initial';
+    organization_name = '';
+    link_to_project = '';
+    first_name = '';
+    last_name = '';
+    email = '';
+  }} />
+{/if}
 <div class="text-[36px] font-Oxanium font-medium uppercase">
   Send request
 </div>
 <div class="text-[#888888]">Fill out the form below and our manager will contact you as soon as possible</div>
-
 
 <form on:submit|preventDefault={onSubmit}>
   <!-- org -->
@@ -78,7 +92,7 @@ const onSubmit = async (e) => {
     <input bind:value={last_name} type="text" id="last-name" required class="my-input mt-[6px]" placeholder="Last name">
   </div>
   <div class="flex justify-end w-full mt-9">
-    <button class="flex justify-center gap-2 md:w-full sm:w-full disabled:opacity-[0.5] disabled:cursor-not-allowed anim-btn rounded-[12px] py-[14px] px-9 bg-[#F97C0E]">
+    <button disabled={requestState === 'loading'} class="flex justify-center gap-2 md:w-full sm:w-full disabled:opacity-[0.5] disabled:cursor-not-allowed anim-btn rounded-[12px] py-[14px] px-9 bg-[#F97C0E]">
       <div>
         <svg xmlns="http://www.w3.org/2000/svg" width="25" height="24" viewBox="0 0 25 24" fill="none">
           <path fill-rule="evenodd" clip-rule="evenodd" d="M22.2076 2.29292C22.4818 2.56708 22.5727 2.975 22.441 3.33967L15.9411 21.3397C15.9317 21.3657 15.9212 21.3914 15.9096 21.4166C15.786 21.6864 15.5875 21.9151 15.3377 22.0754C15.0879 22.2357 14.7974 22.3209 14.5006 22.3209C14.2038 22.3209 13.9132 22.2357 13.6634 22.0754C13.4196 21.9189 13.2246 21.6974 13.1005 21.4359L9.75514 14.7454L3.06468 11.4001C2.80325 11.276 2.58167 11.081 2.4252 10.8372C2.2649 10.5874 2.17969 10.2968 2.17969 10C2.17969 9.70323 2.2649 9.41267 2.4252 9.16288C2.5855 8.9131 2.81414 8.71458 3.08396 8.59094C3.10916 8.57939 3.13484 8.56889 3.16091 8.55947L21.1608 2.05947C21.5255 1.92779 21.9334 2.01876 22.2076 2.29292ZM11.7173 14.1975L14.4327 19.6283L19.037 6.8777L11.7173 14.1975ZM17.6228 5.46352L4.87231 10.0679L10.303 12.7833L17.6228 5.46352Z" fill="white"/>
---
+++ src/pages/get-listed-v2/success-modal.svelte
@@ -0,0 +1,22 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+  import { fade } from "svelte/transition";
+
+  const dispatch = createEventDispatcher();
+  export let email = ''
+</script>
+<div transition:fade class="fixed z-[155] cursor-pointer flex items-center justify-center top-0 left-[50%] translate-x-[-50%] bg-[rgba(0,0,0,0.5)] backdrop-blur-sm w-[100vw] h-[100vh]">
+  <div on:click|stopPropagation class="flex w-[380px] cursor-default z-[160] rounded-[24px] overflow-hidden">
+    <div class="bg-[#2A2A2A] w-full p-6">
+      <div class="text-[#FFFFFF] text-center text-[24px] font-medium">Success!</div>
+      <div class="text-center mt-4 text-[#C1C2C5]">We have sent the registration link to your email:  {email}</div>
+      <svg class="mx-auto mt-6" width="128" height="128" viewBox="0 0 128 128" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <rect width="128" height="128" rx="64" fill="#12B886"/>
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M94.9577 43.7081C96.3463 45.0967 96.3463 47.3479 94.9577 48.7365L59.4022 84.292C58.0136 85.6805 55.7624 85.6805 54.3739 84.292L36.5961 66.5142C35.2076 65.1257 35.2076 62.8745 36.5961 61.4859C37.9846 60.0974 40.2359 60.0974 41.6244 61.4859L56.888 76.7495L89.9294 43.7081C91.3179 42.3196 93.5692 42.3196 94.9577 43.7081Z" fill="white"/>
+      </svg>
+      <button on:click={() => dispatch('close')} class="flex w-full mt-6 justify-center gap-2 md:w-full sm:w-full disabled:opacity-[0.5] disabled:cursor-not-allowed anim-btn rounded-[12px] py-[14px] text-black px-9 bg-[white]">
+        <span>Got it</span>
+      </button>
+    </div>
+  </div>
+</div>
